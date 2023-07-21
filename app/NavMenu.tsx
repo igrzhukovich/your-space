f980651213b3819bdc311dc7427303cd21cefce9
Igor Zhukovich <igrzhukovich@gmail.com>
2023-07-21 11:42:46 +0300

    add about page

--- app/NavMenu.tsx
+++ app/NavMenu.tsx
@@ -2,23 +2,21 @@ import Link from "next/link";
 
 export default function NavMenu() {
     return (
-        <div className="container">
-            <nav>
-                <ul>
-                    <li>
-                        <Link href={"/"}>Home</Link>
-                    </li>
-                    <li>
-                        <Link href={"/about"}>About</Link>
-                    </li>
-                    <li>
-                        <Link href={"/blog"}>Blog</Link>
-                    </li>
-                    <li>
-                        <Link href={"/users"}>Users</Link>
-                    </li>
-                </ul>
-            </nav>
-        </div>
+        <nav>
+            <ul>
+                <li>
+                    <Link href={"/"}>Home</Link>
+                </li>
+                <li>
+                    <Link href={"/about"}>About</Link>
+                </li>
+                <li>
+                    <Link href={"/blog"}>Blog</Link>
+                </li>
+                <li>
+                    <Link href={"/users"}>Users</Link>
+                </li>
+            </ul>
+        </nav>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import styles from "./page.module.css";
-
 export default function Home() {
     return (
-        <main className={styles.main}>
-            <div>Hello!</div>
-        </main>
+        <div>
+            <h2>Hello!</h2>
+        </div>
     );
 }
